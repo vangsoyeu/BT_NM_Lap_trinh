873d6b04ff7f922e1679cba3a9fd8a3a44ab789a
vangsoyeu <cutevcl123456@gmail.com>
2022-11-24 03:32:41 +0700

    BT_buoi_3

--- Bai_tap_buoi_3/ThucHanh.js
+++ Bai_tap_buoi_3/ThucHanh.js
@@ -5,3 +5,12 @@ let area = width*height;
 
 document.write("The area is: " + area)
 }
+
+function hinh_tron() {
+    let r = parseInt(prompt("Bán kính đường tròn"));
+    let perimeter = 2*r*3.14;
+    let area = Math.pow ( r, 2) * 3.14;
+   
+    alert("Chu vi hình tròn: " + perimeter);
+    alert("Diện tích hình tròn: " + area);
+}
